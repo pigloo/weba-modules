8890bfd90e277acf1c3b7e008daf93696be17217
pigloo <busterdrummond@hotmail.com>
2022-06-05 17:28:28 +0100

    change audio file

--- audio/index.js
+++ audio/index.js
@@ -16,7 +16,7 @@ export default () => {
 
   // load a sound and set it as the Audio object's buffer
   const audioLoader = new THREE.AudioLoader();
-  audioLoader.load( baseUrl + '/wind-1.mp3', function( buffer ) {
+  audioLoader.load( baseUrl + '/audio_windy.mp3', function( buffer ) {
     sound.setBuffer( buffer );
     sound.setLoop( true );
     sound.setVolume( 0.5 );
